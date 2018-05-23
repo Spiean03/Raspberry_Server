e0841e1ce89719cb3e038efc3870a40012f8f12b
Andreas Spielhofer <andreas.spielhofer@mail.mcgill.ca>
2018-05-23 08:17:42 -0400

    Update main.py

--- crossfilter/main.py
+++ crossfilter/main.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 16:10:01 2017
+@author:    Andreas Spielhofer
+            Ph.D. Candidate
+            Physics Departement
+            McGill University
+            Montreal, Canada
+@contact:   andreas.spielhofer@mail.mcgill.ca
+"""
+
 import pandas as pd
 import numpy as np
 import datetime
